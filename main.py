e2b7d998b70ee1b0f1ad067393e48ea16bf58a20
status201created <tuannghia.tech@gmail.com>
2025-07-08 22:04:12 +0700

    Path and query parameters

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
   return {"message": "Hello World!"}
+
+class BookCreateModel(BaseModel):
+  title: str
+  author: str
+
+@app.get("/greet")
+async def greet_name(name: Optional[str] = None, age: Optional[int] = None) -> dict:
+  return {"message": f"Hello {name}", "age": age}
+
+@app.post("/books")
+async def create_book(book: BookCreateModel) -> dict:
+  return {"title": book.title, "author": book.author}
